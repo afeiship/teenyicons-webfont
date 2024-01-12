fc93581a56aab46d877703c22d6ecb5c3ad18e79
afeiship <1290657123@qq.com>
2024-01-12 17:42:00 +0800

    feat: add merge py

--- merge.py
+++ merge.py
@@ -1,14 +1,13 @@
 import glob
 import os
-icons_solid = glob.glob('src/solid/*.svg')
-icons_outline = glob.glob('src/outline/*.svg')
+icons_solid = glob.glob('dist/solid/*.svg')
+icons_outline = glob.glob('dist/outline/*.svg')
 
 # prepare dist folder
-os.system('rm -rf dist')
-os.system('mkdir dist')
+os.system('mkdir -p dist/all')
 
 for icon in icons_solid:
-    os.system('cp ' + icon + ' dist/' + icon.split('/')[-1].replace('.svg', '-solid.svg'))
+    os.system('cp ' + icon + ' dist/all/' + icon.split('/')[-1].replace('.svg', '-solid.svg'))
 
 for icon in icons_outline:
-    os.system('cp ' + icon + ' dist/' + icon.split('/')[-1].replace('.svg', '-outline.svg'))
+    os.system('cp ' + icon + ' dist/all/' + icon.split('/')[-1].replace('.svg', '-outline.svg'))
